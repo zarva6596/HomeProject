38f9d73c0286c5490022cc197a54dd85f3d60c4a
= <=>
2021-09-16 10:42:18 +0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,16 @@ module.exports = {
   env: {
     node: true
   },
+
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended'
   ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+  
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
